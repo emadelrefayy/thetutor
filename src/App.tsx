1ce3a9b86745db3283c6422b39a5d39113b1fa03
Hero <emadelrefayy@gmail.com>
2026-08-31 16:04:22 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,36 @@
-import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import {
+  BrowserRouter,
+  Navigate,
+  Route,
+  Routes,
+} from 'react-router-dom';
 
 import GradesPage from './pages/GradesPage';
+import TermsPage from './pages/TermsPage';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Navigate to="/grades" replace />} />
-        <Route path="/grades" element={<GradesPage />} />
-        <Route path="*" element={<Navigate to="/grades" replace />} />
+        <Route
+          path="/"
+          element={<Navigate to="/grades" replace />}
+        />
+
+        <Route
+          path="/grades"
+          element={<GradesPage />}
+        />
+
+        <Route
+          path="/grades/:gradeId/terms"
+          element={<TermsPage />}
+        />
+
+        <Route
+          path="*"
+          element={<Navigate to="/grades" replace />}
+        />
       </Routes>
     </BrowserRouter>
   );
