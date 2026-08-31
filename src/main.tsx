b553f659dd2d7fa3d7cd75ab2b1597603977f8ab
Hero <emadelrefayy@gmail.com>
2026-08-31 13:46:50 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,15 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
 import App from './App';
+import './index.css';
 
-createRoot(document.getElementById('root')!).render(
+const rootElement = document.getElementById('root');
+
+if (!rootElement) {
+  throw new Error('TheTutor root element was not found.');
+}
+
+createRoot(rootElement).render(
   <StrictMode>
     <App />
   </StrictMode>,
