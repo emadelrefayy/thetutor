3c1bac0a33484f3f8d147698f41f3884dbf317dc
Hero <emadelrefayy@gmail.com>
2026-08-31 13:44:32 +0300

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { BrowserRouter } from 'react-router-dom';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div id="app">
+        <h1>TheTutor</h1>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
